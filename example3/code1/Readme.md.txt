79dcf9fde9aed1babfd45fcba733ab80ed9c15f3
kallelzied <kallelzied@live.com>
2018-03-13 22:56:32 +0100

    add test to example3

---
+++ example3/code1/Readme.md.txt
@@ -0,0 +1,9 @@
+Test with promesses
+
+1) npm init
+2) npm install --save mocha
+3) npm install --save chai
+4) npm install --save chai-as-promised
+5) mocha ./test/**/*.spec.js
+
+# you can use npm test instead of mocha ./test/**/*.spec.js since we specified it in package.json
---
+++ example3/code1/test/controllers/auth.controller.spec.js
@@ -0,0 +1,43 @@
+var assert = require('assert');
+var authController = require('../../controllers/auth.controller');
+var expect = require('chai').expect;
+var should = require('chai').should();
+var chai = require('chai');
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
+
+describe('AuthController', function(){
+    beforeEach('this function initialized Roles', function settingUpRoles(){
+        console.log('running before each');
+        authController.setRoles(['user']);
+    });
+    describe('isAuthorized', function(){
+        
+        it('Should return false if not authorized', function(){
+            var isAuth = authController.isAuthorized('admin');
+            isAuth.should.be.false;
+        })
+        it('Should return true if authorized', function () {
+            authController.setRoles(['user', 'admin']);
+            var isAuth = authController.isAuthorized('admin');
+            isAuth.should.be.true;
+        })
+        it('should not allow a get if not authorized');
+        it('should allow get if authorized');
+    })
+    describe('isAuthorizedAsync', function(){
+        it('Should return false if not authorized', function(done){
+            authController.isAuthorizedAsync('admin', 
+            function(isAuth){
+                isAuth.should.be.false;
+                done();
+            });
+        })
+    })
+    describe('isAuthorizedPromise', function(){
+        it('Should return false if not authorized', function(){
+            return authController.isAuthorizedPromise('admin').should.eventually.be.false;
+        })
+    })
+});
